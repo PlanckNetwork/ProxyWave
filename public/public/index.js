491df6ff345d6d116d055abbfcd081b5223aba1e
SpeedSlicer <mathewshaun25@gmail.com>
2025-03-10 13:12:15 -0400

    url setter

--- public/public/index.js
+++ public/public/index.js
@@ -42,3 +42,20 @@ form.addEventListener("submit", async (event) => {
 	}
 	frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
+async function searchURL(url) {
+	try {
+		await registerSW();
+	} catch (err) {
+		error.textContent = "Failed to register service worker.";
+		errorCode.textContent = err.toString();
+		throw err;
+	}
+
+	let frame = document.getElementById("uv-frame");
+	frame.style.display = "block";
+	let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+	if (await connection.getTransport() !== "/epoxy/index.mjs") {
+		await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
+	}
+	frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+}
