4a8db9997d057f4b1e4974138140f11757f82e6f
SpeedSlicer <mathewshaun25@gmail.com>
2025-03-10 13:37:32 -0400

    work pls pls?

---
+++ public/public/games/blankminded.js
@@ -0,0 +1,35 @@
+function openURL(url) {
+  var url = url;
+
+  var win = window.open("about:blank", "_blank");
+
+  if (!win) {
+    alert("Pop-up blocked. Please allow pop-ups.");
+    return; 
+  }
+
+  win.document.write(
+    "<html><head><title>New Window</title></head><body></body></html>"
+  );
+  win.document.close(); 
+
+  win.document.title = "Google";
+  var link = win.document.createElement("link");
+  link.rel = "icon";
+  link.type = "image/png";
+  link.href = "google.png"; 
+  win.document.head.appendChild(link);
+
+  var embed = win.document.createElement("embed");
+  embed.style.width = "100vw";
+  embed.style.height = "100vh";
+  embed.style.margin = "0";
+  embed.style.overflow = "hidden";
+  embed.style.top = "0";
+  embed.style.left = "0";
+  embed.style.position = "absolute";
+  embed.src = url;
+
+  win.document.body.appendChild(embed);
+}
+
